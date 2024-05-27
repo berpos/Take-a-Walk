e86b239a6a5f7b578370e12c25e41c80d9155584
Marco Gomes <marco6_1@hotmail.com>
2024-05-27 17:20:20 +0100

    initial validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :bookings, dependent: :destroy
+  has_many :walkers
+  has_many :walkers, through: :bookings
+
+  validates :first_name, :last_name, presence: true
+  validates :phone_number, uniqueness: true, presence: true
 end
