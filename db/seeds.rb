af9d66f97e31eb95dd4cc9d9efd2e4bd29184b08
bernardo campos <berposdzn@gmail.com>
2024-05-27 17:38:05 +0100

    Walkers home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'cleaning database'
+Walker.destroy_all
+puts 'creating new database'
+10.times do
+  Walker.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    phone_number: Faker::PhoneNumber.phone_number,
+  )
+end
+puts 'New databse created'
