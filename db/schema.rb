12d19ad18ec39316ee60d66ee92fed67b38267c6
Marco Gomes <marco6_1@hotmail.com>
2024-05-31 12:22:00 +0100

    add dynamic avatar/profile pic, add stylling

--- db/schema.rb
+++ db/schema.rb
@@ -78,12 +78,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_30_134922) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_walkers_on_user_id"
     t.string "description"
     t.string "address"
     t.float "latitude"
     t.float "longitude"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_walkers_on_user_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
